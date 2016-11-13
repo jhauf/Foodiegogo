bfea796ce49d4128bc464c6f2ec0eab85098b6b8
Jane Hauf <janehauf@janes-mbp.attlocal.net>
2016-11-13 10:20:01 -0800

    update splash

--- frontend/components/campaigns/campaign_index.jsx
+++ frontend/components/campaigns/campaign_index.jsx
@@ -10,6 +10,7 @@ class CampaignIndex extends React.Component {
 
   render() {
     return <div>
+      <h1 className="header">Campaigns</h1>
       <div className="categories">
         <button onClick={this.props.applyFilter(BAKED_GOODS)}> Baked Goods </button>
         <button onClick={this.props.applyFilter(RESTAURANT)}> Restaurants </button>
@@ -19,7 +20,6 @@ class CampaignIndex extends React.Component {
         <button onClick={this.props.applyFilter("")}> All Campaigns </button>
       </div>
       <SearchContainer/>
-      <h1 className="header">Campaigns</h1>
       <ul className="campaignIndex">
         {
           this.props.campaigns.map(campaign => (
