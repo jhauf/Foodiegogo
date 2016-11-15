e730066fb8f44e3253ae2946f8b43240e58d54d6
Jane Hauf <janehauf@Janes-MacBook-Pro.local>
2016-11-15 14:29:55 -0800

    fix font

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,7 @@
-const path = require('path');
 module.exports = {
-  context: __dirname,
-  entry: './frontend/foodiegogo.jsx',
+  entry: "./lib/igloo.js",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
+  	filename: "./lib/bundle.js"
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  },
-  devtool: 'source-maps'
+  devtool: 'source-map',
 };
